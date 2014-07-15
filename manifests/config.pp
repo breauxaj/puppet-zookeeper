013bd80b7b716efab1f52bb4da1beb8f41ebd01a
Adam Breaux <breauxaj@gmail.com>
2014-07-15 10:20:23 -0400

    manage the zoo.cfg file

---
+++ manifests/config.pp
@@ -0,0 +1,35 @@
+define zookeeper::config (
+  $ticktime = '2000',
+  $initlimit = '10',
+  $synclimit = '5',
+  $datadir = '/var/zookeeper',
+  $clientport = '2181',
+  $maxclientcnxns = '',
+  $autopurge_snapretaincount = '',
+  $autopurge_purgeinterval = ''
+) {
+  $config = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => '/usr/local/zookeeper/conf/zoo.cfg',
+  }
+
+  $service = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'zookeeper' ],
+  }
+
+  file { $config:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('zookeeper/zoo.erb'),
+    notify  => Service[$service],
+  }
+
+  file { $datadir:
+    ensure => directory,
+    owner  => 'zookeeper',
+    group  => 'zookeeper',
+    mode   => '0755',
+  }
+
+}
