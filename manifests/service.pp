3658c7114fe814ea729554699ba008e180606eca
Adam Breaux <breauxaj@gmail.com>
2014-07-15 10:21:12 -0400

    stage the init script as part of the service

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,12 +3,21 @@ define zookeeper::service (
   $enable = true
 ) {
   $service = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'zookeeper' ],
+    /(?i-mx:centos|fedora|redhat|scientific)/ => 'zookeeper',
   }
 
   service { $service:
     ensure  => $ensure,
     enable  => $enable,
+    require => File['/etc/init.d/zookeeper']
+  }
+
+  file { '/etc/init.d/zookeeper':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/zookeeper/zookeeper.init'
   }
 
 }
