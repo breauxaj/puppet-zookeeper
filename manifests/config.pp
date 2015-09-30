70dfef051581e24b5abbf16d665981318a5bd1bf
Adam Breaux <breauxaj@gmail.com>
2015-09-30 10:25:24 -0400

    only manage sysconfig
    the rest should be in version control

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,59 +1,16 @@
 define zookeeper::config (
-  $ticktime = '2000',
-  $initlimit = '5',
-  $synclimit = '2',
-  $datadir = '/var/zookeeper',
-  $clientport = '2181',
-  $maxclientcnxns = '',
-  $autopurge_snapretaincount = '',
-  $autopurge_purgeinterval = '',
-  $myid = '1',
-  $servers = ''
+  $value
 ) {
-  $config = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => '/usr/local/zookeeper/conf/zoo.cfg',
-  }
+  include ::zookeeper
 
-  $log4j = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => '/usr/local/zookeeper/conf/log4j.properties',
-  }
+  $key = $title
 
-  $service = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'zookeeper' ],
-  }
+  $context = '/files/etc/sysconfig/zookeeper'
 
-  file { $config:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('zookeeper/zoo.erb'),
-    notify  => Service[$service],
-  }
-
-  file { $log4j:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('zookeeper/log4j.erb'),
-    notify  => Service[$service],
-  }
-
-  file { $datadir:
-    ensure => directory,
-    owner  => 'zookeeper',
-    group  => 'zookeeper',
-    mode   => '0755',
-  }
-
-  file { "${datadir}/myid":
-    ensure  => present,
-    owner   => 'zookeeper',
-    group   => 'zookeeper',
-    mode    => '0644',
-    content => $myid,
-    require => File[$datadir],
+  augeas { "zoo_sysconfig/${key}":
+    context => $context,
+    onlyif  => "get ${key} != '${value}'",
+    changes => "set ${key} '${value}'",
   }
 
 }
