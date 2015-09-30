b8b2858220419a76a982c4efc56b036333ab1fc5
Adam Breaux <breauxaj@gmail.com>
2015-09-30 10:14:52 -0400

    cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class zookeeper (
   $version = '3.4.6',
   $source = 'http://www.us.apache.org/dist/zookeeper/stable/'
 ){
-  exec { 'get-server':
+  exec { 'get-zoo':
     path    => '/bin:/usr/bin',
     command => "wget ${source}/zookeeper-${version}.tar.gz",
     cwd     => '/tmp',
@@ -11,13 +11,13 @@ class zookeeper (
     onlyif  => "test ! -d /usr/local/zookeeper-${version}",
   }
 
-  exec { 'untar-server':
+  exec { 'untar-zoo':
     path    => '/bin:/usr/bin',
     command => "tar -zxf /tmp/zookeeper-${version}.tar.gz",
     cwd     => '/usr/local',
     creates => "/usr/local/zookeeper-${version}",
     timeout => 10000,
-    require => Exec['get-server'],
+    require => Exec['get-zoo'],
   }
 
   file { "/usr/local/zookeeper-${version}":
@@ -25,7 +25,7 @@ class zookeeper (
     owner   => 'root',
     group   => 'root',
     recurse => true,
-    require => Exec['untar-server'],
+    require => Exec['untar-zoo'],
   }
 
   file { '/usr/local/zookeeper':
@@ -33,15 +33,7 @@ class zookeeper (
     owner   => 'root',
     group   => 'root',
     target  => "/usr/local/zookeeper-${version}",
-    require => Exec['untar-server'],
-  }
-
-  file { '/etc/profile.d/zookeeper.sh':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/zookeeper/profile.txt',
+    require => Exec['untar-zoo'],
   }
 
   group { 'zookeeper':
