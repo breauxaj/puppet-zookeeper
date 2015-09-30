7eb16c4c170a671d0f0e0576bdd941e2be2875ce
Adam Breaux <breauxaj@gmail.com>
2015-09-30 09:57:23 -0400

    rhel7 support

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,23 +1,62 @@
 define zookeeper::service (
-  $ensure = running,
-  $enable = true
+  $ensure,
+  $enable
 ) {
+  include ::zookeeper
+
   $service = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'zookeeper',
   }
 
-  service { $service:
-    ensure  => $ensure,
-    enable  => $enable,
-    require => File['/etc/init.d/zookeeper']
-  }
+  case $::osfamily {
+    'redhat': {
+      case $::lsbmajdistrelease {
+        '6': {
+          service { $service:
+            ensure  => $ensure,
+            enable  => $enable,
+            require => File['/etc/init.d/zookeeper']
+          }
 
-  file { '/etc/init.d/zookeeper':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/zookeeper/zookeeper.init'
+          file { '/etc/init.d/zookeeper':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            source => 'puppet:///modules/zookeeper/zookeeper.init'
+          }
+        }
+        default: {
+          include ::systemctl
+
+          service { $service:
+            ensure  => $ensure,
+            enable  => $enable,
+            require => File['/lib/systemd/system/zookeeper.service']
+          }
+
+          file { '/lib/systemd/system/zookeeper.service':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            source => 'puppet:///modules/zookeeper/zookeeper.service'
+          }
+
+          file { '/etc/systemd/system/zookeeper.service':
+            ensure  => 'link',
+            owner   => 'root',
+            group   => 'root',
+            target  => '/lib/systemd/system/zookeeper.service'
+          }
+
+          systemctl::reload { 'zookeeper': }
+        }
+      }
+    }
+    default: {
+      notify { 'OS not supported by this module': }
+    }
   }
 
 }
